a1078b44bfdc29440af877fad6ab9e7f7f2f9f92
Embarkupon <165702416+Embarkupon@users.noreply.github.com>
2024-08-21 18:59:45 -0400

    Finished the game rules and logic, the game shows the results at the end of each round. It's rock, paper, scissors.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
 var humanScore = 0;
 var computerScore = 0;
-var score = 0;
 var playing = true;
 
+/** while loop calls the playRound function until the player cancels the prompt. */
 while(playing) {
     playRound(getHumanChoice(), getComputerChoice());
 }
+
 /** function getComputerChoice generates a random number between 1 and 3.
  *  That number is then put through a switch statement, with each case returning
  *  either 'rock', 'paper', or 'scissors' depending on the number.
@@ -43,9 +44,9 @@ function getHumanChoice() {
         let enumerated = 0;
         while(playerChoice.toLowerCase() != 'rock' && playerChoice.toLowerCase() != 'paper' && playerChoice.toLowerCase() != 'scissors') {
             if (enumerated < 3) {
-                playerChoice = prompt("Will you play 'Rock', 'Paper' or 'Scissors'?");
+                playerChoice = prompt("Sorry, I didn't understand.\nWill you play 'Rock', 'Paper' or 'Scissors'?");
             } else {
-                playerChoice = prompt("Please select 'Rock', 'Paper' or 'Scissors'.");
+                playerChoice = prompt("Please type one of the three choices:\n 'Rock', 'Paper' or 'Scissors'.");
             }
             enumerated++;
         }
@@ -56,13 +57,62 @@ function getHumanChoice() {
     return playerChoice;
 }
 
-/** */
+/** The playRound functions plays takes the player's and computer's choices and
+ *  compares them to determine who wins. Depending on whether the player wins, loses
+ *  or the game ends in a draw, a boolean flag is set and determines the outcome
+ *  of the round. At the end of the round (function) the results will be displayed
+ *  and the scoreboard will be updated.
+*/
 function playRound(humanChoice, computerChoice) {
+    let playerWon = true;
+    let draw = false;
+    let play = humanChoice.toLowerCase();
+    console.log(`You chose ${play}`);
     if (humanChoice == 'cancel') {
-        console.log('exiting play loop.')
+        alert(`Thanks for playing!\nYour final score is ${humanScore}\nComputer's final score is ${computerScore}`);
+        console.log('exiting game loop.')
+        return;
     } else {
-        console.log(humanChoice);
-        score++;
+        console.log(`Computer chose ${computerChoice}`);
+        switch(computerChoice) {
+            case 'rock':
+                if (play == 'rock') {       
+                    draw = true;
+                } else if (play == 'paper') {
+                    playerWon = true;
+                } else if (play == 'scissors') {
+                    playerWon = false;
+                }
+                break;
+            case 'paper':
+                if (play == 'rock') {
+                    playerWon = false;
+                } else if (play == 'paper') {
+                    draw = true;
+                } else if (play == 'scissors') {
+                    playerWon = true;
+                }
+                break;
+            case 'scissors':
+                if (play == 'rock') {
+                    playerWon = true;
+                } else if (play == 'paper') {
+                    playerWon = false;
+                } else if (play == 'scissors') {
+                    draw = true;
+                }
+                break;
+            default:
+                break;
+        }
     }
-    console.log(`Your score is ${score}`);
+    if (playerWon && !draw) {
+        humanScore++;
+        alert(`You won! ${play} beats ${computerChoice}!\nPlayer got a point!\nYour score is ${humanScore}\nComputer score is ${computerScore}`);
+    } else if(draw) {
+        alert(`It's a Draw, you both chose ${play}!\nThe score didn't change.\nYour score is ${humanScore}\nComputer score is ${computerScore}`);
+    } else {
+        computerScore++;
+        alert(`You lost! ${computerChoice} beats ${play}.\nComputer got a point!\nYour score is ${humanScore}\nComputer score is ${computerScore}`);
+    }
 }
